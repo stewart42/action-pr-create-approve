d5c5365f1b6c968e14f6ccc48423589e97892c8d
Stewart Scott <stewart42@gmail.com>
2020-09-29 11:45:22 +0200

    basic action setup

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,51 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {context, getOctokit} from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    core.info('Validating')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const token = core.getInput('GITHUB_TOKEN', {required: true})
+    const title = core.getInput('title', {required: true})
+    const head = core.getInput('head', {required: true})
+    const base = core.getInput('base', {required: true})
 
-    core.setOutput('time', new Date().toTimeString())
+    const {owner, repo} = context.repo
+    core.debug(`owner: ${owner} repo: ${repo}`)
+
+    const octokit = getOctokit(token)
+    const response = await octokit.pulls.create({
+      owner,
+      repo,
+      title,
+      head,
+      base
+    })
+
+    const pull_number = response.data.id
+    core.info(`Created PR: ${pull_number}`)
+
+    await octokit.pulls.createReview({
+      owner,
+      repo,
+      pull_number,
+      event: 'APPROVE'
+    })
+
+    core.info(`Approved PR: ${pull_number}`)
+
+    if (response.data.mergeable) {
+      await octokit.pulls.merge({
+        owner,
+        repo,
+        pull_number,
+        merge_method: 'squash'
+      })
+    }
+
+    core.setOutput('pull_number', pull_number.toString())
   } catch (error) {
+    core.debug(error)
     core.setFailed(error.message)
   }
 }
